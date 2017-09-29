f382f85d684416e4943edac1090e6215aae070b7
NuttinGo <nico2carr@gmail.com>
2017-09-29 16:11:16 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/nico/lo52_badminton/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nico.lo52_badminton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
